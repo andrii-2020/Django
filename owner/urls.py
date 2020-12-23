d18e473b1f54ae4809a60451394e721eec003764
andrii-2020 <bebion58@gmail.com>
2020-12-23 18:38:53 +0100

    calculator

--- owner/urls.py
+++ owner/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from .views import owner, animals, home
+from .views import owner, animals, home, cal
 urlpatterns = [
     path('', home),
+    path('<int:calc><str:string><int:cal>', cal, name='calcu'),
     path('own', owner),
-    path('anim', animals)
+    path('anim', animals),
+
+
 ]
--- owner/views.py
+++ owner/views.py
@@ -7,15 +7,33 @@ from time import gmtime, strftime
 
 def home(request):
     # %Y - %m - %d %H:%M:%S
-    t = strftime("%m - %d - %Y", gmtime())
+    t = strftime("%m-%d-%Y", gmtime())
     return render(request, 'homee.html', {'t': t})
 
 
 def owner(request):
     qs = Owner.objects.all()
+    qs = qs.order_by('name')
     return render(request, 'owner.html', {'owner': qs})
 
 
 def animals(request):
     qs1 = Animals.objects.all()
+    qs1 = qs1.order_by('name')
     return render(request, 'animals.html', {'animals': qs1})
+
+
+def cal(request, calc, string, cal):
+    if string == 'add':
+        res = calc.__add__(cal)
+        string = '+'
+    elif string == 'sub':
+        string = '-'
+        res = calc.__sub__(cal)
+    elif string == 'mul':
+        string = '*'
+        res = calc.__mul__(cal)
+
+    return render(request, 'calcu.html', {'res': res, 'calc': calc, 'cal': cal, 'str': string})
+
+
